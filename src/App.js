259d1f0ac2047888586d3c9606a1ab5080bc4932
Harshal Laheri <harshal@harshallaheri.me>
2024-05-23 21:42:53 -0400

    added delete modal

--- src/App.js
+++ src/App.js
@@ -5,41 +5,52 @@ import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const todosList = [
-    { id: 1, text: "Buy eggs" },
-    { id: 2, text: "Walk the dog" },
-    { id: 3, text: "Watch a movie" },
-  ];
+    const todosList = [
+        { id: 1, text: "Buy eggs" },
+        { id: 2, text: "Walk the dog" },
+        { id: 3, text: "Watch a movie" },
+    ];
 
-  const [todos, setTodos] = useState(todosList);
+    const [todos, setTodos] = useState(todosList);
 
-  function deleteTodo(id) {
-    // IMPLEMENT DELETE TODO
-  }
+    function deleteTodo(id) {
+        // IMPLEMENT DELETE TODO
+        const temp = todos.filter((todo) => todo.id !== id);
+        setTodos(temp);
+    }
 
-  function addTodo(newTodo) {
-    // IMPLEMENT ADD TODOS
-  }
+    function addTodo(newTodo) {
+        // IMPLEMENT ADD TODOS
+        const temp = [...todos, newTodo];
+        setTodos(temp);
+    }
 
-  function editTodo(id, updatedTodo) {
-    // IMPLEMENT EDIT TODO
-    // setTodos(updatedItem);
-  }
+    function editTodo(id, updatedTodo) {
+        // IMPLEMENT EDIT TODO
+        const updatedTodos = todos.map((todo) =>
+            todo.id === id ? updatedTodo : todo
+        );
+        setTodos(updatedTodos);
+    }
 
-  return (
-    <VStack p={5}>
-      <Text
-        bgGradient="red"
-        bgClip="text"
-        fontSize="6xl"
-        fontWeight="extrabold"
-      >
-        Todo App
-      </Text>
-      <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
-      <AddTodo addTodo={addTodo} />
-    </VStack>
-  );
+    return (
+        <VStack p={5}>
+            <Text
+                bgGradient="red"
+                bgClip="text"
+                fontSize="6xl"
+                fontWeight="extrabold"
+            >
+                Todo App
+            </Text>
+            <TodoList
+                todos={todos}
+                deleteTodo={deleteTodo}
+                editTodo={editTodo}
+            />
+            <AddTodo addTodo={addTodo} />
+        </VStack>
+    );
 }
 
 export default App;
